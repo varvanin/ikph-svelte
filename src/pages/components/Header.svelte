c3cd9d00624eb865d1ad2d895f73f6565fe62ca0
Yevhen Varvanin <varvanin@iMac-Yevgen.local>
2020-04-12 18:53:15 +0200

    menu fix

--- src/pages/components/Header.svelte
+++ src/pages/components/Header.svelte
@@ -133,14 +133,17 @@
     background-color: #eef1f4;
     position: fixed;
     display: block;
-    height: calc(100% - 90px);
+    height: 40px;
+    float: right;
     bottom: 0;
     left: 0;
+    top: 75px;
   }
 
   .navbar-list li {
     list-style-type: none;
     position: relative;
+    float: left;
   }
 
   .navbar-list li:before {
@@ -182,15 +185,20 @@
       display: flex;
       padding: 0;
     }
-
-    .navbar-list a {
-      display: inline-flex;
-    }
   }
 
   @media (max-width: 767px) {
     .logo {
       margin: 0.5em;
+    }
+    .navbar-list a {
+      display: inline-flex;
+      padding: 0 4px;
+      font-size: 12px;
+    }
+
+    .navbar-list {
+      padding: 0 10px;
     }
   }
 </style>
